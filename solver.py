d858cabe8fc715ae946365336797978962d9f469
Demilade Oyedele <demilade.oyedele@gmail.com>
2021-06-13 00:10:59 +0100

    Adding a function to find empty boxes in the board

--- solver.py
+++ solver.py
@@ -13,7 +13,7 @@ board = [
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+            print("- - - - - - - - - - - - ")
 
         for j in range(len(bo[0])):
             if j % 3 == 0 and j != 0:
@@ -24,4 +24,8 @@ def print_board(bo):
                 else:
                     print(str(bo[i][j]) + " ", end="")
 
-print_board(board)
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j) # row, col
